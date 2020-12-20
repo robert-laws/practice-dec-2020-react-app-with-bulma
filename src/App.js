ba44e0be901b8ecae7299eb1f1653a361896b094
Robert Laws <roblaws@me.com>
2020-12-20 13:11:15 +0300

    set up some basic examples

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
+import { useState } from 'react';
 import './App.scss';
 
 function App() {
+  const [ddActive, setDdActive] = useState(false);
   return (
     <div>
       <h1 className='mb-2'>App</h1>
-      <a href='/' class='button is-primary'>
+      <a href='/' className='button is-primary'>
         Primary
       </a>
+      <span className='icon has-text-info'>
+        <i className='fas fa-info-circle'></i>
+      </span>
+
+      <div className={`dropdown ${ddActive ? 'is-active' : ''}`}>
+        <div className='dropdown-trigger'>
+          <button
+            className='button'
+            aria-haspopup='true'
+            aria-controls='dropdown-menu'
+            onClick={() => setDdActive(!ddActive)}
+          >
+            <span>Dropdown button</span>
+            <span className='icon is-small'>
+              <i className='fas fa-angle-down' aria-hidden='true'></i>
+            </span>
+          </button>
+        </div>
+        <div className='dropdown-menu' id='dropdown-menu' role='menu'>
+          <div className='dropdown-content'>
+            <a href='/' className='dropdown-item'>
+              Dropdown item
+            </a>
+            <a href='/' className='dropdown-item'>
+              Other dropdown item
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
