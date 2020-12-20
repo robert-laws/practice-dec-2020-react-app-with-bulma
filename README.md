b0812f0b7a0004f1f66ab9b75452d9162b062064
Robert Laws <roblaws@me.com>
2020-12-20 12:31:34 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Practice - December 2020 - React App with Bulma
+
+React App setup using Bulma
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>App</h1>
+    </div>
+  );
+}
+
+export default App;
