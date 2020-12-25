119016a4623dfb9aa59a6045d020de449fd48c6b
Robert Laws <roblaws@me.com>
2020-12-25 16:44:25 +0300

    add close button to modal header

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -11,60 +11,78 @@ export const Modal = () => {
         className='modal-background'
         onClick={() => updateModal({ group: null, obj: null })}
       ></div>
-      <div
-        className='modal-content has-background-white py-5 px-5'
-        style={{ borderRadius: '0.25rem' }}
-      >
-        <h3 className='title'>
-          {selectionObject ? selectionObject.title : ''}
-        </h3>
-        {selectionGroup && selectionGroup === 'locations' && (
-          <div className='box'>
-            <p>
-              Address:
-              <br />
-              {selectionObject ? `${selectionObject.street}` : ''}
-              <br />
-              {selectionObject
-                ? `${selectionObject.city}, ${selectionObject.state} ${selectionObject.zip}`
-                : ''}
-            </p>
-            <hr />
-            <p>
-              Location Type:{' '}
-              {selectionObject ? selectionObject.location_type : ''}
-            </p>
-            <p>
-              Opening Times: {selectionObject ? selectionObject.open_time : ''}{' '}
-              - {selectionObject ? selectionObject.close_time : ''}
-            </p>
-            {selectionObject ? (
+      <div className='modal-card'>
+        <header class='modal-card-head'>
+          <p class='modal-card-title'>
+            {selectionGroup && selectionGroup === 'locations'
+              ? 'Location Information'
+              : 'Menu Item Details'}
+          </p>
+          <button
+            class='delete'
+            aria-label='close'
+            onClick={() => updateModal({ group: null, obj: null })}
+          ></button>
+        </header>
+        <section
+          className='modal-card-body has-background-white py-5 px-5'
+          style={{
+            borderBottomLeftRadius: '0.25rem',
+            borderBottomRightRadius: '0.25rem',
+          }}
+        >
+          <h3 className='title'>
+            {selectionObject ? selectionObject.title : ''}
+          </h3>
+          {selectionGroup && selectionGroup === 'locations' && (
+            <div className='box'>
               <p>
-                {selectionObject.drive_thru
-                  ? '* Drive Thru available at this location'
+                Address:
+                <br />
+                {selectionObject ? `${selectionObject.street}` : ''}
+                <br />
+                {selectionObject
+                  ? `${selectionObject.city}, ${selectionObject.state} ${selectionObject.zip}`
                   : ''}
               </p>
-            ) : (
-              ''
-            )}
-          </div>
-        )}
-        {selectionGroup && selectionGroup === 'menus' && (
-          <div className='box'>
-            <p>Category: {selectionObject ? selectionObject.category : ''}</p>
-            <hr />
-            <p>Price: ${selectionObject ? selectionObject.price : ''}</p>
-            {selectionObject ? (
-              selectionObject.seasonal ? (
-                <p>Available Seasonally</p>
+              <hr />
+              <p>
+                Location Type:{' '}
+                {selectionObject ? selectionObject.location_type : ''}
+              </p>
+              <p>
+                Opening Times:{' '}
+                {selectionObject ? selectionObject.open_time : ''} -{' '}
+                {selectionObject ? selectionObject.close_time : ''}
+              </p>
+              {selectionObject ? (
+                <p>
+                  {selectionObject.drive_thru
+                    ? '* Drive Thru available at this location'
+                    : ''}
+                </p>
               ) : (
                 ''
-              )
-            ) : (
-              ''
-            )}
-          </div>
-        )}
+              )}
+            </div>
+          )}
+          {selectionGroup && selectionGroup === 'menus' && (
+            <div className='box'>
+              <p>Category: {selectionObject ? selectionObject.category : ''}</p>
+              <hr />
+              <p>Price: ${selectionObject ? selectionObject.price : ''}</p>
+              {selectionObject ? (
+                selectionObject.seasonal ? (
+                  <p>Available Seasonally</p>
+                ) : (
+                  ''
+                )
+              ) : (
+                ''
+              )}
+            </div>
+          )}
+        </section>
       </div>
     </div>
   );
