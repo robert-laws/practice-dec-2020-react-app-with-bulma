2fcb6629a75305884920501adce716e9fa7520b7
Robert Laws <roblaws@me.com>
2020-12-31 15:59:12 +0300

    begin work on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,58 +1,71 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Slide from 'react-reveal/Slide';
 import hero from '../images/data-viz-cafe-hero.jpg';
+import paperCupArtistic from '../images/home-page/paper-cup-artistic.png';
 
 export const Home = () => {
-  const [progressVal, setProgressVal] = useState(0);
-
-  const changeProgress = (value) => {
-    if (progressVal < 100) {
-      setProgressVal(progressVal + value);
-    }
-  };
-
   return (
-    <section className='section'>
-      <div className='container'>
-        <div className='columns is-vcentered'>
-          <div className='column is-one-third'>
-            <h1 className='title is-size-3-mobile is-size-1-desktop ml-2'>
-              Data Viz Cafe
-            </h1>
-            <h3
-              className='title is-size-5-mobile is-size-4-desktop ml-2'
-              style={{ marginTop: '-1rem' }}
-            >
-              Resources for Learning Data Visualization
-            </h3>
-          </div>
-          <div className='column'>
-            <img src={hero} alt='Hero for Data Visualization' />
+    <>
+      <section className='section'>
+        <div className='container'>
+          <div className='columns is-vcentered'>
+            <div className='column is-one-third'>
+              <h1 className='title is-size-3-mobile is-size-1-desktop ml-2'>
+                Data Viz Cafe
+              </h1>
+              <h3
+                className='title is-size-5-mobile is-size-4-desktop ml-2'
+                style={{ marginTop: '-1rem' }}
+              >
+                Resources for Learning Data Visualization
+              </h3>
+            </div>
+            <div className='column'>
+              <img src={hero} alt='Hero for Data Visualization' />
+            </div>
           </div>
         </div>
+      </section>
+      <div
+        className='my-6 has-background-warning-light'
+        style={{ minHeight: '700px' }}
+      >
+        <section className='section'>
+          <div className='container'>
+            <div className='columns'>
+              <div className='column is-three-fifths is-offset-one-fifth'>
+                <h2 className='subtitle is-size-2-desktop'>Text...</h2>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+      <section className='section'>
         <div className='container'>
           <div className='columns'>
-            <div className='column is-one-third'>Card...</div>
-            <div className='column is-one-third'>
-              <progress
-                className='progress is-primary'
-                style={{ transition: 'width 0.5s ease' }}
-                value={progressVal}
-                max='100'
-              >
-                15%
-              </progress>
-            </div>
-            <div className='column is-one-third'>
-              <button
-                className='button is-success'
-                onClick={() => changeProgress(10)}
-              >
-                Increase by 10
-              </button>
+            <div className='column is-three-fifths is-offset-one-fifth'>
+              <h2 className='subtitle is-size-2-desktop'>
+                <Slide big left>
+                  <h1 className='title is-size-2-desktop has-text-centered'>
+                    Big Taste and Style
+                  </h1>
+                  <div className='is-flex is-flex-direction-row is-justify-content-center'>
+                    <img
+                      className='image mx-5'
+                      style={{ height: '400px' }}
+                      src={paperCupArtistic}
+                      alt='Paper Coffee Cup'
+                    />
+                    <div className='is-flex is-flex-direction-column is-justify-content-center mx-5'>
+                      <div className='kpi'>24 Menu Choices</div>
+                    </div>
+                  </div>
+                </Slide>
+              </h2>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
