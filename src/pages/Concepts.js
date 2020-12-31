badef6b48ac2151521edd5ad72c4f66d9f55b17f
Robert Laws <roblaws@me.com>
2020-12-31 18:02:43 +0300

    begin work on concepts page

--- src/pages/Concepts.js
+++ src/pages/Concepts.js
@@ -6,6 +6,14 @@ export const Concepts = () => {
   return (
     <SubPage>
       <PageTitle>Data Visualization Concepts</PageTitle>
+      <div className='container mx-2'>
+        <div className='block'>
+          <p>concepts...</p>
+        </div>
+        <div className='block'>
+          <p>concepts...</p>
+        </div>
+      </div>
     </SubPage>
   );
 };
