1f299beffc47f8135f74a9c4ae32a575c851c85e
Robert Laws <roblaws@me.com>
2021-01-03 11:30:49 +0300

    update footer content

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,20 +1,31 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { ReactComponent as Logo } from '../images/data-viz-cafe-logo-02.svg';
 
 export const Footer = () => {
   return (
     <footer
       className='footer is-flex-grow-0 has-background-dark'
-      style={{ minHeight: '12rem' }}
+      style={{ maxHeight: '24rem' }}
     >
       <div className='columns has-text-white'>
-        <div className='column'></div>
-        <div className='column has-text-centered'>
-          <p>Data Viz Cafe</p>
-        </div>
-        <div className='column has-text-centered'>
+        <div className='column is-4'></div>
+        <div className='column is-2 mt-3'>
+          <NavLink to='/'>
+            <p style={{ color: 'white' }} className='has-text-weight-semibold'>
+              The Data Viz Cafe
+            </p>
+          </NavLink>
+          <p>Course: Spring 2021</p>
+          <p>Fictitious company from Connecticut</p>
           <p>Copyright 2021</p>
         </div>
-        <div className='column'></div>
+        <div className='column is-2 has-text-centered'>
+          <NavLink to='/'>
+            <Logo alt='Data Viz Cafe' width='116' height='116' />
+          </NavLink>
+        </div>
+        <div className='column is-4'></div>
       </div>
     </footer>
   );
