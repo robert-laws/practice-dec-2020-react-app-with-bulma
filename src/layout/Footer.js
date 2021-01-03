5c4fb38264b919e76b588e666e54a9d8e40db44b
Robert Laws <roblaws@me.com>
2021-01-03 18:59:13 +0300

    return to top on click to home page from footer

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { ReactComponent as Logo } from '../images/data-viz-cafe-logo-02.svg';
 
 export const Footer = () => {
+  const history = useHistory();
+
+  const returnHome = () => {
+    history.push('/');
+    window.scrollTo(0, 0);
+  };
+
   return (
     <footer
       className='footer is-flex-grow-0 has-background-dark'
@@ -11,20 +18,26 @@ export const Footer = () => {
       <div className='columns has-text-white'>
         <div className='column is-4'></div>
         <div className='column is-2 mt-3'>
-          <NavLink to='/'>
-            <p style={{ color: 'white' }} className='has-text-weight-semibold'>
-              The Data Viz Cafe
-            </p>
-          </NavLink>
+          <p
+            style={{ color: 'white', cursor: 'pointer' }}
+            className='has-text-weight-semibold'
+            onClick={returnHome}
+          >
+            The Data Viz Cafe
+          </p>
+
           <p>Course: Spring 2021</p>
           <p>Fictitious Coffee Company</p>
           <p>Connecticut, USA</p>
           <p>Copyright 2021</p>
         </div>
         <div className='column is-2 has-text-centered'>
-          <NavLink to='/'>
-            <Logo alt='Data Viz Cafe' width='116' height='116' />
-          </NavLink>
+          <Logo
+            alt='Data Viz Cafe'
+            width='116'
+            height='116'
+            onClick={returnHome}
+          />
         </div>
         <div className='column is-4'></div>
       </div>
