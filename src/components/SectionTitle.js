7b433b3b5c447e400e48549e78a9c98a4bead4bc
Robert Laws <roblaws@me.com>
2021-01-03 16:45:40 +0300

    adjust title and subtitle size for mobile

--- src/components/SectionTitle.js
+++ src/components/SectionTitle.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 export const SectionTitle = ({ id = '', children }) => {
   return (
-    <h1 id={id} className='subtitle is-size-5-desktop mt-5 mb-1 ml-2'>
+    <h1
+      id={id}
+      className='subtitle is-size-5-desktop is-size-5-mobile mt-5 mb-1 ml-2'
+    >
       {children}
     </h1>
   );
--- src/components/Subtitle.js
+++ src/components/Subtitle.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
 export const Subtitle = ({ children }) => {
-  return <h1 className='subtitle is-size-3-desktop'>{children}</h1>;
+  return (
+    <h1 className='subtitle is-size-3-desktop is-size-4-mobile'>{children}</h1>
+  );
 };
