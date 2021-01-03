cd2bf302c54add064829c3a4101225f4a565e70e
Robert Laws <roblaws@me.com>
2021-01-03 11:46:09 +0300

    add to concepts page

--- src/pages/Concepts.js
+++ src/pages/Concepts.js
@@ -7,11 +7,22 @@ export const Concepts = () => {
     <SubPage>
       <PageTitle>Data Visualization Concepts</PageTitle>
       <div className='container mx-2'>
-        <div className='block'>
-          <p>concepts...</p>
-        </div>
-        <div className='block'>
-          <p>concepts...</p>
+        <div className='content ml-4'>
+          <p className='is-size-5'>
+            <span className='has-text-weight-bold'>Data Visualization</span> -
+            is the graphical representation of information and data.
+          </p>
+          <p className='is-size-5'>
+            <span className='has-text-weight-bold'>Data set</span> - is a
+            collection of data. In the case of tabular data, a data set
+            corresponds to one or more database tables.
+          </p>
+          <p className='is-size-5'>
+            <span className='has-text-weight-bold'>Categorical Data</span>
+          </p>
+          <p className='is-size-5'>
+            <span className='has-text-weight-bold'>Numerical Data</span>
+          </p>
         </div>
       </div>
     </SubPage>
