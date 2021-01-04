0570f51ebe0d8d2d9e1a9ce7ee06e31e8600654f
Robert Laws <roblaws@me.com>
2021-01-04 10:18:15 +0300

    add concepts content

--- src/pages/Concepts.js
+++ src/pages/Concepts.js
@@ -8,21 +8,52 @@ export const Concepts = () => {
       <PageTitle>Data Visualization Concepts</PageTitle>
       <div className='container mx-2'>
         <div className='content ml-4'>
-          <p className='is-size-5'>
-            <span className='has-text-weight-bold'>Data Visualization</span> -
-            is the graphical representation of information and data.
-          </p>
-          <p className='is-size-5'>
-            <span className='has-text-weight-bold'>Data set</span> - is a
-            collection of data. In the case of tabular data, a data set
-            corresponds to one or more database tables.
-          </p>
-          <p className='is-size-5'>
-            <span className='has-text-weight-bold'>Categorical Data</span>
-          </p>
-          <p className='is-size-5'>
-            <span className='has-text-weight-bold'>Numerical Data</span>
-          </p>
+          <div className='box'>
+            <h4 className='is-size-4-desktop is-size-4-mobile'>
+              General Concepts
+            </h4>
+            <p className='is-size-5 ml-3'>
+              <span className='has-text-weight-bold'>Data Visualization</span> -
+              is the graphical representation of information and data.
+            </p>
+            <p className='is-size-5 ml-3'>
+              <span className='has-text-weight-bold'>Data set</span> - is a
+              collection of data. In the case of tabular data, a data set
+              corresponds to one or more database tables.
+            </p>
+            <p className='is-size-5 ml-3'>
+              <span className='has-text-weight-bold'>Categorical Data</span> -
+              represent types of data which may be divided into groups
+            </p>
+            <p className='is-size-5 ml-3'>
+              <span className='has-text-weight-bold'>Numerical Data</span> -
+              expressed in numbers that can have arithmetic operations performed
+              on them
+            </p>
+          </div>
+          <div className='box'>
+            <h4 className='is-size-4-desktop is-size-4-mobile'>
+              Tableau Concepts
+            </h4>
+            <p className='is-size-5 ml-3'>
+              <span className='has-text-weight-bold'>Dimensions</span> - contain
+              qualitative values (such as names, dates, or geographical data).
+            </p>
+            <p className='is-size-5 ml-3'>
+              <span className='has-text-weight-bold'>Measures</span> - contain
+              numeric, quantitative values that you can measure.
+            </p>
+            <p className='is-size-5 ml-3'>
+              <span className='has-text-weight-bold'>Discrete</span> - counted
+              as distinct and separate and can only take individual values
+              within a range.
+            </p>
+            <p className='is-size-5 ml-3'>
+              <span className='has-text-weight-bold'>Continuous</span> - are
+              values that have an order over a continuum that can’t be
+              re-ordered.
+            </p>
+          </div>
         </div>
       </div>
     </SubPage>
